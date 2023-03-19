fd4e6c051563664d9b726151973f553e9431de8a
ddrag23 <kurodyami@gmail.com>
2023-03-19 14:13:47 +0700

    remove duplicate user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
+import { UserController } from './user/user.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
-import { UserController } from './user.controller';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [UserModule, AuthModule,],
